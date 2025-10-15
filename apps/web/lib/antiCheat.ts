ada81c8e5d9f07f9e5e7e596242065fdde55672d
apkbusinessflr-stack <apkbusinessflr@gmail.com>
2025-10-15 22:19:59 +0300

    reset

--- apps/web/lib/antiCheat.ts
+++
@@ -1,5 +0,0 @@
-export function isHumanLatency(ms: number) { return ms >= 60; }
-export function anomalyFlags(reactions: number[]) {
-  const tooManyFast = reactions.filter(v => v < 120).length > reactions.length * 0.7;
-  return { tooManyFast };
-}
